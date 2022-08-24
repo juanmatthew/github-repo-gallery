478829cb975186c71fba249690ef467c312ca1df
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-08-24 14:10:22 -0400

    Fetched the API json Data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 const overview = document.querySelector(".overview");
 //GitHub username
 const username = "juanmatthew";
+
+const gitUsers = async function () {
+    //Target the “users” endpoint and use a template literal to add the global username variable to the endpoint: users/${username}. Notice that you’ll add a “$” character in front of the variable name to create a placeholder. Because you’re using a template literal, surround the URL in backticks instead of quotation marks.
+  const userData = await fetch(`https://api.github.com/users/${username}`); 
+  //In your next await statement, resolve the JSON response.
+  const userInfo = await userData.json();
+  //Log out the response to the console 
+  console.log(userInfo);
+};
+//call your function to see your results
+gitUsers();
+//In the response, look carefully at the properties. You’ll need some of those properties to complete the next function!
